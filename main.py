2a570b400d0476e617b10f2e57fa100468ccabf0
mohamedfayznada-jpg <mohamedfayznada@gmail.com>
2026-05-30 06:19:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 bot = Bot(token=BOT_TOKEN)
-
 async def main():
-    await bot.send_message(
-        chat_id=CHAT_ID,
-        text="✅ البوت شغال بنجاح"
+    await client.start()
+
+    channel = await client.get_entity("yasminstoriii")
+
+    messages = await client.get_messages(
+        channel,
+        limit=5
     )
 
+    for i, msg in enumerate(messages):
+        print("=" * 50)
+        print("MESSAGE", i + 1)
+        print("ID:", msg.id)
+        print("TEXT:", repr(msg.message))
+        print("MEDIA:", msg.media is not None)
 asyncio.run(main())
