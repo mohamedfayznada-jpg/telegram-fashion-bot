7a9da020aa8fe462dbb3750fdd86547a632e6e4e
mohamedfayznada-jpg <mohamedfayznada@gmail.com>
2026-05-30 21:18:17 +0300

    Create send_to_telegram.py

---
+++ send_to_telegram.py
@@ -0,0 +1,55 @@
+import os
+import requests
+
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+caption = ""
+
+files_to_read = [
+    "facebook_post_soft.txt",
+    "facebook_post_sales.txt",
+    "facebook_post_viral.txt",
+    "story_post.txt"
+]
+
+for file in files_to_read:
+
+    if os.path.exists(file):
+
+        caption += f"\n\n===== {file} =====\n\n"
+
+        with open(
+            file,
+            "r",
+            encoding="utf-8"
+        ) as f:
+
+            caption += f.read()
+
+requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": caption[:4000]
+    }
+)
+
+if os.path.exists("marketing_collage.jpg"):
+
+    with open(
+        "marketing_collage.jpg",
+        "rb"
+    ) as photo:
+
+        requests.post(
+            f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto",
+            data={
+                "chat_id": CHAT_ID
+            },
+            files={
+                "photo": photo
+            }
+        )
+
+print("SENT TO TELEGRAM")
