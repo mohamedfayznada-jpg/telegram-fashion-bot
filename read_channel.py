a4294a98b3b0119bcf4051cb65f8c9aa2f07e7d0
mohamedfayznada-jpg <mohamedfayznada@gmail.com>
2026-05-30 06:27:08 +0300

    Update read_channel.py

--- read_channel.py
+++ read_channel.py
@@ -1,7 +1,29 @@
+import os
+import base64
+from telethon import TelegramClient
+
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
+
+session_data = base64.b64decode(
+    os.environ["TELEGRAM_SESSION"]
+)
+
+with open("telegram_session.session", "wb") as f:
+    f.write(session_data)
+
+client = TelegramClient(
+    "telegram_session",
+    API_ID,
+    API_HASH
+)
+
 async def main():
     await client.start()
 
-    channel = await client.get_entity("yasminstoriii")
+    channel = await client.get_entity(
+        "yasminstoriii"
+    )
 
     messages = await client.get_messages(
         channel,
@@ -14,3 +36,6 @@ async def main():
         print("ID:", msg.id)
         print("TEXT:", repr(msg.message))
         print("MEDIA:", msg.media is not None)
+
+with client:
+    client.loop.run_until_complete(main())
