08befb8b1a04f98747f1bb42fdd41b63fbedffe1
mohamedfayznada-jpg <mohamedfayznada@gmail.com>
2026-05-31 16:09:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import os
-from telegram import Bot
+from telethon import TelegramClient
 import asyncio
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
 
-bot = Bot(token=BOT_TOKEN)
 async def main():
+    # استخدام Telethon بدلاً من python-telegram-bot عشان ميبقاش في تعارض
+    client = TelegramClient("telegram_session", API_ID, API_HASH)
     await client.start()
 
     channel = await client.get_entity("yasminstoriii")
-
-    messages = await client.get_messages(
-        channel,
-        limit=5
-    )
+    messages = await client.get_messages(channel, limit=5)
 
     for i, msg in enumerate(messages):
         print("=" * 50)
@@ -22,4 +19,8 @@ async def main():
         print("ID:", msg.id)
         print("TEXT:", repr(msg.message))
         print("MEDIA:", msg.media is not None)
-asyncio.run(main())
+        
+    await client.disconnect()
+
+if __name__ == "__main__":
+    asyncio.run(main())
